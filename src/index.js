37407d1f69e7f1604923742088c4582766855449
ottoyd <ugaipribadi@gmail.com>
2022-08-31 17:05:02 +0700

    change

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://ottoyd-graphql-server.herokuapp.com/", // change to YOUR own production server
-  cache: new InMemoryCache(),
-  name: "web",
-  version: "1.0",
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
